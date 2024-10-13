e25dc1b3c24a1bc43326c68130dcee541f585dcb
Prathmesh Gandhale <125843181+Shadowgit98@users.noreply.github.com>
2024-10-13 12:51:50 +0530

    first commit

---
+++ src/app/data.js
@@ -0,0 +1,74 @@
+/*
+Websites:
+
+- https://github.com/pmndrs/gltfjsx (GLTF JSX for 3D Models)
+- https://lucide.dev/icons/ (Lucide Icons)
+- https://github.com/anuraghazra/github-readme-stats (Github Readme Stats)
+- https://skillicons.dev (Skill Icons to show skills)
+- https://github-readme-streak-stats.herokuapp.com (Github Readme Streak Stats)
+
+:root {
+  --background: 27 27 27;
+  --foreground: 225 225 225;
+  --muted: 115 115 115;
+  --accent: 254 254 91; #FEFE5B
+}
+
+*/
+
+export const projectsData = [
+  {
+    id: 1,
+    name: "2D Portfolio",
+    description: "Interact with my portfolio in a simulator",
+    date: "2024-02-25",
+    demoLink: "https://github.com/Shadowgit98/2D-portfolio-kaboom",
+  },
+  {
+    id: 2,
+    name: "ArtGallery Online",
+    description: "Digital art showcase platform",
+    date: "2022-06-20",
+    demoLink: "https://artgalleryonline.example.com",
+  },
+  {
+    id: 3,
+    name: "MindfulMoments",
+    description: "Meditation and mindfulness app",
+    date: "2022-02-14",
+    demoLink: "https://mindfulmoments.example.com",
+  },
+];
+
+export const BtnList = [
+  { label: "Home", link: "/", icon: "home", newTab: false },
+  { label: "About", link: "/about", icon: "about", newTab: false },
+  { label: "Projects", link: "/projects", icon: "projects", newTab: false },
+  { label: "Contact", link: "/contact", icon: "contact", newTab: false },
+  {
+    label: "Github",
+    link: "https://github.com/Shadowgit98",
+    icon: "github",
+    newTab: true,
+  },
+  {
+    label: "LinkedIn",
+    link: "https://www.linkedin.com/in/prathmesh-gandhale-44421a203/",
+    icon: "linkedin",
+    newTab: true,
+  },
+  {
+    label: "X",
+    link: "https://x.com/Prathmesh19998",
+    icon: "twitter",
+    newTab: true,
+  },
+  {
+    label: "Resume",
+    link: "/CV developer.pdf",
+    icon: "resume",
+    newTab: true,
+  },
+];
+
+
